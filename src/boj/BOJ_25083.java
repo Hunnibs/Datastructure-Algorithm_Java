2315dc9f353233ef93c60e65d4a257aac10d8899
unknown <cqqudgjs@hufs.ac.kr>
2023-07-14 13:45:07 +0900

    Bronze

---
+++ src/boj/BOJ_25083.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class BOJ_25083 {
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
---
+++ src/boj/BOJ_27866.java
@@ -0,0 +1,22 @@
+package day3;
+
+import java.util.Scanner;
+
+public class BOJ_27866 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String name = sc.next();
+		int N = sc.nextInt();
+		String[] arr = new String[name.length()];
+		
+		for (int i = 0; i < name.length(); i++) {
+			arr[i] = Character.toString(name.charAt(i));
+		}
+		
+		System.out.println(arr[N-1]);
+	}
+
+}
