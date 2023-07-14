9f6928d6c6a98e24a5ae01cc7c089fd221716635
unknown <cqqudgjs@hufs.ac.kr>
2023-07-14 15:49:52 +0900

    Bronze

---
+++ src/boj/BOJ_10809.java
@@ -0,0 +1,33 @@
+package day3;
+
+import java.util.Scanner;
+
+public class BOJ_10809 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String S = sc.next();
+		char[] s_arr = new char[S.length()];
+		int[] arr = new int[26];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = -1;
+		}
+		
+		for (int i = 0; i < S.length(); i++) {
+			s_arr[i] = S.charAt(i);
+		}
+		
+		for (int i = 0; i < s_arr.length; i++) {
+			if (arr[s_arr[i]-97] == -1) {
+				arr[s_arr[i]-97] = i;
+			} else {
+				continue;
+			}
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");			
+		}
+	}
+}
