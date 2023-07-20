94384a9dd0bb8628b521e891e2bf64e1be268d08
Hunnibs <106607029+Hunnibs@users.noreply.github.com>
2023-07-20 19:29:59 +0900

    error

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String words = "안녕하세요 저는 이병헌입니다";
+        char word = words.charAt(1);
+
+        for (int i = 0; i < words.length(); i++){
+            System.out.println(words.charAt(i));
+        }
+
+        StringTokenizer st = new StringTokenizer(words);
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
     }
 }
