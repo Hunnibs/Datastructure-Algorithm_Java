a5a080c0961126f16f5f79c887fa7791772732a1
Hunnibs <106607029+Hunnibs@users.noreply.github.com>
2023-07-25 20:55:40 +0900

    Bronze

---
+++ src/boj/BOJ_01259.java
@@ -0,0 +1,29 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_01259 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        String num = st.nextToken();
+        while (!num.equals("0")){
+            String answer = "yes";
+            for(int i = 0; i < num.length() / 2; i++){
+                if (num.charAt(i) != num.charAt(num.length()-1-i)){
+                    answer = "no";
+                    break;
+                }
+            }
+            System.out.println(answer);
+
+            st = new StringTokenizer(br.readLine());
+            num = st.nextToken();
+        }
+    }
+}
---
+++ src/boj/BOJ_15829.java
@@ -0,0 +1,30 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15829 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static final int M = 1234567891;
+    static int N;
+    static String words;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        words = st.nextToken();
+
+        long sum = 0;
+        long pow = 1;
+        for(int i =0; i< N; i++){
+            sum += ((words.charAt(i) - 'a' + 1) * pow % M);
+            pow = pow * 31 % M;
+        }
+        System.out.println(sum % M);
+    }
+}
