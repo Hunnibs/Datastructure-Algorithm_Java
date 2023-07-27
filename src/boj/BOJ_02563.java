448cb4f3dab3890fb18a4f7a2178bd569aa3f676
devDachan <chn7894@naver.com>
2023-07-27 15:28:50 +0900

    Silver_TwoDimensionArray

---
+++ src/boj/BOJ_02563.java
@@ -0,0 +1,38 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_02563 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		int[][] board = new int[100][100];
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int col = Integer.parseInt(st.nextToken()), row = Integer.parseInt(st.nextToken());
+			for (int r = row; r < row+10; r++) {
+				for (int c = col; c < col+10; c++) {
+					board[r][c] = 1;
+				}
+			}
+		}
+		
+		int sum = 0;
+		for (int r = 0; r < 100; r++) {
+			for (int c = 0; c < 100; c++) {
+				if (board[r][c] == 1) {
+					sum += 1;
+				}
+			}
+		}
+		System.out.println(sum);
+	}
+}
