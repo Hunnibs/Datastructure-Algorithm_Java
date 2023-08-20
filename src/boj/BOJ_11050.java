f7e2957bb60d311022895bd9039e2df6378f7eb3
Hunnibs <106607029+Hunnibs@users.noreply.github.com>
2023-08-20 18:04:36 +0900

    s

---
+++ src/boj/BOJ_11050.java
@@ -0,0 +1,38 @@
+package boj;
+
+import java.util.Scanner;
+
+/**
+
+@author 이병헌
+@since 2023. 08. 19
+@see https://www.acmicpc.net/problem/11050
+@git
+@youtube
+@performance
+@category # Combination
+@note */
+
+public class BOJ_11050 {
+    static int N, R, answer;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        R = sc.nextInt();
+
+        combination(0, 0);
+
+        System.out.println(answer);
+    }
+
+    private static void combination(int start, int depth){
+        if (depth == R){
+            answer++;
+            return;
+        } else{
+            for (int i = start; i < N; i++){
+                combination(i+1, depth+1);
+            }
+        }
+    }
+}
--- src/swea/Hamster.java
+++ src/swea/Hamster.java
@@ -3,42 +3,50 @@ package swea;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Hamster {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
-	
-	static int N, X, M, r, c;
-	static int[][] dp;
-	static int[] answer;
-	
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		st = new StringTokenizer(br.readLine());
-		int T = Integer.parseInt(st.nextToken());
-		for (int test_case = 1; test_case < T; test_case++) {
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			X = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
-			
-			dp = new int[N+1][N+1];
-			for (int r = 0; r < N+1; r++) {
-				Arrays.fill(dp, -1);
-			}
-			
-			for (int i = 0; i < M; i++) {
-				st = new StringTokenizer(br.readLine());
-				r = Integer.parseInt(st.nextToken());
-				c = Integer.parseInt(st.nextToken());
-				
-				dp[r][c] = Integer.parseInt(st.nextToken()); 
-			}
-			
-		}
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
 
+    static int T, N, X, M;
+    static int l, r, s;
+    static int[] sums;
+    static int[] answer;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        T = Integer.parseInt(st.nextToken());
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());  // 우리
+            X = Integer.parseInt(st.nextToken());  // 우리 당 최대 햄스터 수
+            M = Integer.parseInt(st.nextToken());  // 기록 개수
+            answer = new int[N];
+            sums = new int[N+1];
+
+            for (int m = 0; m < M; m++){
+                st = new StringTokenizer(br.readLine());
+                l = Integer.parseInt(st.nextToken());
+                r = Integer.parseInt(st.nextToken());
+                s = Integer.parseInt(st.nextToken());
+
+            }
+        }
+    }
+
+    static void permutation(int depth){
+        if (depth == N) {
+
+            return;
+        } else{
+            for (int i = 0; i < X; i++){
+                answer[depth] = i;
+                sums[depth] = sums[depth-1] + i;
+                permutation(depth+1);
+            }
+        }
+    }
 }
