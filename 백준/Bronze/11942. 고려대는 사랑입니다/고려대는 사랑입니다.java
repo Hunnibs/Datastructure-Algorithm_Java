9f467db23b13aa0f0eabeea55f3112c1b7ed3669
Hunnibs <106607029+Hunnibs@users.noreply.github.com>
2023-09-19 23:51:51 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 72 ms, Memory: 11420 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        System.out.println("고려대학교");
+    }
+}
