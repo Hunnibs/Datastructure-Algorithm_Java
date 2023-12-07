d94d175765cbb61853f28d8077ae19b2acc460bd
Hunnibs <106607029+Hunnibs@users.noreply.github.com>
2023-12-07 15:41:18 +0900

    [Gold V] Title: 머리 톡톡, Time: 1900 ms, Memory: 53572 KB -BaekjoonHub

---
+++ 백준/Gold/1241. 머리 톡톡/머리 톡톡.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+
+- @author 이병헌
+- @since 12/7/23
+- @see https://www.acmicpc.net/problem/1241
+- @git https://github.com/Hunnibs
+- @youtube
+- @performance 2sec, 128MB
+- @category # Dynamic Programming # Memorization
+- @note
+ 배수를 +1씩 업데이트해준다.
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] list = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            list[i] = num;
+        }
+
+        int[] dp = new int[1_000_001];
+        for (int i = 0; i < N; i++) {
+            dp[list[i]]++;
+        }
+
+        int[] ans = new int[N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j * j <= list[i]; j++) {
+                if (list[i] % j == 0) {
+                    if (list[i] / j == j){
+                        ans[i] += dp[j];
+                    } else{
+                        ans[i] += dp[j] + dp[list[i]/ j];
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(ans[i]-1);
+        }
+    }
+}
