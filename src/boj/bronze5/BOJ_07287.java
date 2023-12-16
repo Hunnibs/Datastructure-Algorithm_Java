54ceac3a0b59ecffe11618e3a2ee680206df7801
hunnibs <hunnibs97@gmail.com>
2023-12-16 20:20:39 +0900

    2023.12.16 - Sprout

---
+++ src/boj/bronze5/BOJ_07287.java
@@ -0,0 +1,10 @@
+package boj.bronze5;
+
+import java.io.BufferedReader;
+
+public class BOJ_07287 {
+    public static void main(String[] args) {
+        System.out.println(253);
+        System.out.println("hunnibs");
+    }
+}
