3f20be200a7aefca13c4905c9cbef2aa7c2417eb
hunnibs <hunnibs97@gmail.com>
2023-12-23 15:25:24 +0900

    2023.12.23 - Implementation

---
+++ src/boj/bronze3/BOJ_30958.java
@@ -0,0 +1,24 @@
+package boj.bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_30958 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+
+        int[] num = new int[26];
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) - 'a' <= 26 && S.charAt(i) - 'a' >= 0){
+                num[S.charAt(i) - 'a']++;
+            }
+        }
+
+        System.out.println(Arrays.stream(num).max().getAsInt());
+    }
+}
