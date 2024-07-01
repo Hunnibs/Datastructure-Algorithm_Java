19ed963ead7f34b0869ad74bdecd08c72db7f380
Hunnibs <106607029+Hunnibs@users.noreply.github.com>
2024-07-01 11:41:43 +0900

    all update

--- src/boj/bronze5/BOJ_02558.java
+++ src/boj/bronze5/BOJ_02558.java
@@ -1,2 +1,13 @@
-package boj.bronze5;public class BOJ_02558 {
+package boj.bronze5;
+
+import java.io.*;
+
+public class BOJ_02558 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.print(A + B);
+    }
 }
