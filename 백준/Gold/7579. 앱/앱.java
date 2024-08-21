150467d32733e3772610f17eb50e47502f91d700
Hunnibs <106607029+Hunnibs@users.noreply.github.com>
2024-08-21 13:51:47 +0900

    [Gold III] Title: 앱, Time: 112 ms, Memory: 16512 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -33,33 +33,21 @@ public class Main {
             costs[i] = Integer.parseInt(st.nextToken());
         }
 
-        PriorityQueue<Info> apps = new PriorityQueue<>(new Comparator<Info>() {
-            @Override
-            public int compare(Info o1, Info o2) {
-                return Integer.compare(o1.cost, o2.cost);
-            }
-        });
-
-        for(int i = 0; i < N; i++){
-            apps.add(new Info(memories[i], costs[i]));
-        }
-
-        knapsack(N, M, apps);
+        knapsack(N, M, costs, memories);
     }
 
-    private static void knapsack(int N, int M, PriorityQueue<Info> apps){
+    private static void knapsack(int N, int M, int[] costs, int[] memories){
         int[][] dp = new int[N+1][10_001];
         int ans = 10_000_000;
 
         for(int i = 1; i <= N; i++){
-            Info cur = apps.poll();
-            dp[i][cur.cost] = cur.memory;
+            dp[i][costs[i-1]] = memories[i-1];
             for(int j = 0; j <= 10_000; j++){
-                if (j < cur.cost){
+                if (j < costs[i-1]){
                     dp[i][j] = dp[i-1][j];
                 }
                 else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cur.cost] + cur.memory);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - costs[i-1]] + memories[i-1]);
                 }
 
                 if (dp[i][j] >= M) {
@@ -69,14 +57,5 @@ public class Main {
         }
 
         System.out.print(ans);
-    }
-
-    private static class Info{
-        int memory, cost;
-
-        public Info(int memory, int cost) {
-            this.memory = memory;
-            this.cost = cost;
-        }
     }
 }
