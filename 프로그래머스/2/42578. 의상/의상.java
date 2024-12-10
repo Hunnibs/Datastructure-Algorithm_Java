fe90814d92552f9ad4e20e51982f31ca2dda12ad
Hunnibs <106607029+Hunnibs@users.noreply.github.com>
2024-12-10 10:09:47 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,23 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 0;
+        int answer = 1;
         
         int size = clothes.length;
         Map<String, Integer> types = new HashMap<>();
-        List<String> type = new ArrayList<>();
         for (int i = 0; i < size; i++){
             String cloth = clothes[i][1];
             if (!types.containsKey(cloth)) types.put(cloth, 1);
             else types.put(cloth, types.get(cloth) + 1);
-            
-            if (!type.contains(cloth)) type.add(cloth);
         }
         
-        if (type.size() != 0) answer = 1;
-        
-        for (String tmp : type){
-            answer *= (types.get(tmp) + 1);
+        Iterator<Integer> it = types.values().iterator();
+        while (it.hasNext()){
+            answer *= (it.next() + 1);
         }
         
         return answer-1;
